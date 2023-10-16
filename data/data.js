275e8c46746a5d16114b44cbf9b98a97e5286b72
JohnWesley14 <johnwesleyrvaz@gmail.com>
2023-10-16 08:14:42 -0300

    data /

---
+++ data/data.js
@@ -0,0 +1,10 @@
+export const informacoes = [
+  paper = {
+    title: "Reciclar lixo orgânico",
+    image: {
+      uri: 'https://www.cbsaparasdepapel.com.br/imagens/informacoes/empresa-reciclagem-papel-06.jpg'
+    },
+    descricao: "Clique para aprender como reciclar papel"
+
+  }
+]
